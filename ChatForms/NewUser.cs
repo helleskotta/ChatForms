d38c308c364a4224def3c18167217bf00966ee26
Helen Luu <helleskotta@gmail.com>
2017-08-23 16:37:17 +0200

    Lagt till lite basic saker i NewUser för att kunna skapa en användare

--- ChatForms/NewUser.cs
+++ ChatForms/NewUser.cs
@@ -12,9 +12,33 @@ namespace ChatForms
 {
     public partial class NewUser : Form
     {
+        User user;
+        Client client;
+
         public NewUser()
         {
             InitializeComponent();
+        }
+
+        public DialogResult ShowLoginForm(User user, Client client)
+        {
+            this.user = user;
+            this.client = client;
+            return ShowDialog();
+        }
+
+        private void CreateUserButton_Click(object sender, EventArgs e)
+        {
+            if (usernameBox.Text.Length > 1 && passwordBox.Text.Length > 3 && passwordBox.Text == passwordBox2.Text)
+            {
+                user.Username = usernameBox.Text;
+                user.Password = passwordBox.Text;
+                //client.Create(user.Username, user.Password);
+            }
+            else
+            {
+                // Validering??? 
+            }
         }
     }
 }
